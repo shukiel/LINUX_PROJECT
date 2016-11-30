058b4dad6afa60ebc9df00d860c67463d5173564
enoshcohen <enoshc@gmail.com>
2016-12-01 00:56:13 +0200

    cmp works fine

--- cmp.pl
+++ cmp.pl
@@ -13,6 +13,8 @@ open(my $f2, '<', $ARGV[1]) || die "Cannot open file $ARGV[1]\n";
 my $b1;
 my $b2;
 my $count = 0;
+my $d1;
+my $d2;
 
 while (1)
 {
@@ -20,8 +22,13 @@ while (1)
 	read ($f2, $b2, 1) or die "EOF on $ARGV[1]" ;
 
 	#print $b1 eq $b2 ? color("yellow") : color("red") ,++$count."\t".ord($b1)." - \Q$b1\E\t".ord($b2)." - \Q$b2\E\n", color("reset");
-	$b1 eq $b2 ? color("yellow") : color("red");
-	colored ( printf("%-5s : %-3s %-2s \t %-3s %-2s\n",++$count, ord($b1), $b1, ord($b2), $b2 ), $b1 eq $b2 ? "yellow" : "red" );
-	color("reset");
+	$d1 = ord($b1);
+	$d2 = ord($b2);
+	
+	$b1 =~ s/(\s)//;
+	$b2 =~ s/(\s)//;
+
+	my $color = $d1 == $d2 ? "reset" : "red";
+	print colored(sprintf("%-5s : %-3s %-2s \t %-3s %-2s\n",++$count, $d1, $b1, $d2, $b2 ), $color);
 }
 
